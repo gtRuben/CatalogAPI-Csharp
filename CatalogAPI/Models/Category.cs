df46d4b6ea7e9029ee84fe73da6d5f7353625f2c
Ruben Melo <rubenmelo02@gmail.com>
2022-11-17 16:10:00 -0300

    Add relationship Category - Product.

--- CatalogAPI/Models/Category.cs
+++ CatalogAPI/Models/Category.cs
@@ -1,8 +1,11 @@
-﻿namespace CatalogAPI.Models;
+﻿using System.Collections.ObjectModel;
+
+namespace CatalogAPI.Models;
 
 public class Category
 {
     public int Id { get; set; }
     public string? Name { get; set; }
     public string? ImageUrl { get; set; }
+    public ICollection<Product>? Products { get; set; } = new Collection<Product>();
 }
