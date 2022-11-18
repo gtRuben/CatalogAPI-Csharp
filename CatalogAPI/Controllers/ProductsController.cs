665046018837b3d1331c24f2a12c65b2e93bf20f
Ruben Melo <rubenmelo02@gmail.com>
2022-11-18 20:48:35 -0300

    Add ProductsController and first Get method.

---
+++ CatalogAPI/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using CatalogAPI.Context;
+using CatalogAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly CatalogAPIContext _context;
+
+        public ProductsController(CatalogAPIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            var products = _context.Products.ToList();
+            return (products == null) ? NotFound() : Ok(products);
+        }
+    }
+}
