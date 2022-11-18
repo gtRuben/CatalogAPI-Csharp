e269f762729c68a0a81389b3d6e2c1afca744071
Ruben Melo <rubenmelo02@gmail.com>
2022-11-18 18:45:28 -0300

    Add migrations to populate DB.

---
+++ CatalogAPI/Migrations/20221118203940_Populate Categories.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogAPI.Migrations
+{
+    public partial class PopulateCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "Insert into Categories(Name, ImageUrl) values " +
+                "row('Drinks','drinks.jpg')," +
+                "row('Snacks', 'snacks.jpg')," +
+                "row('Desserts', 'desserts.jpg')"
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Categories");
+        }
+    }
+}
---
+++ CatalogAPI/Migrations/20221118205009_Populate Products.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogAPI.Migrations
+{
+    public partial class PopulateProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "Insert into Products(Name, Description, Price, ImageUrl, Inventory, RegistrationDate, CategoryId) values " +
+                "row('Conke', '500ml', 5.99, 'conke.jpg', 10, now(), 1)," +
+                "row('Chicken Nugger', '500g', 12.99, 'chicnug.jpg', 3, now(), 2)," +
+                "row('Brownie', 'every meal 200g', 800.00, 'brownie.jpg', 8, now(), 3)"
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Products");
+        }
+    }
+}
